b9a9654c910fe590a6d0d1291000dfb71e650d2e
FrancescoErcole <149020166+FrancescoErcole@users.noreply.github.com>
2024-07-23 09:40:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
-import helpers as h
+import src
 
-x = h.simplex(max=True,Z = [3,5])
+x = src.simplex(max=True,Z = [3,5])
 x.add_constraint((1,0),4,op="<=")
 x.add_constraint((0,2),12,op="<=")
 x.add_constraint((3,2),18,op="<=")
